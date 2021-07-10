d04513df411b3e71893fd771b29f10321bdfa492
Samiul-Islam-Niloy <samiul3030@gmail.com>
2021-07-10 22:23:26 +0600

    Latest

---
+++ AutoCorner/src/autocorner/AutoCorner.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autocorner;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class AutoCorner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new LoginFetch().setVisible(true);
+    }
+    
+}
